02f303e58de966de8c2b436922a1b73fa317a86b
Tenclar <tenclarvalus@gmail.com>
2020-04-21 20:12:21 -0500

    repositórios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {}, []);
   async function handleAddRepository() {
-    // TODO
+    const resp = await api.post("repositories", {
+      title: "Titulo",
+      url: "http://www.github.com/tenclar",
+      techs: ["nodejs", "reactjs"],
+    });
+
+    setRepositories([response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
